aa63b2ec3d8f36c4b4275346be0910178dd6a118
Iván Fernández Gómez <149154386+IvanFernande@users.noreply.github.com>
2024-05-27 15:12:20 +0200

    raspberry-pi1.py

--- raspberry-pi1.py
+++ raspberry-pi1.py
@@ -1,4 +1,3 @@
-import RPi.GPIO as gpio
 import smbus
 import time
 import sys
@@ -7,20 +6,16 @@ bus = smbus.SMBus(1)
 address = 0x04
 
 def main():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(17, gpio.OUT)
     status = False
     while 1:
-        gpio.output(17, status)
         status = not status
         bus.write_byte(address, 1 if status else 0)
-        print "Arduino answer to RPI:", bus.read_byte(address)
+        print("Arduino answer to RPI:", bus.read_byte(address))
         time.sleep(1)
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print 'Interrupted'
-        gpio.cleanup()
+        print('Interrupted')
         sys.exit(0)
