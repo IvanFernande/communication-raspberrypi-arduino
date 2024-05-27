794a834a8731d45036db5e13b647f41f440c7dc5
Iván Fernández Gómez <149154386+IvanFernande@users.noreply.github.com>
2024-05-27 15:03:11 +0200

    raspberry-pi.py

--- raspberry-pi.py
+++ raspberry-pi.py
@@ -1,53 +1,26 @@
-#i2cdetect -y 1
-#library
-import sys
-import smbus2 as smbus#,smbus2
+import RPi.GPIO as gpio
+import smbus
 import time
-# Slave Addresses
-I2C_SLAVE_ADDRESS = 11 #0x0b ou 11
-I2C_SLAVE2_ADDRESS = 12
-I2C_SLAVE3_ADDRESS = 13
-# This function converts a string to an array of bytes.
-def ConvertStringsToBytes(src):
-  converted = []
-  for b in src:
-    converted.append(ord(b))
-  return converted
-def main(args):
-    # Create the I2C bus
-    I2Cbus = smbus.SMBus(1)
-    with smbus.SMBus(1) as I2Cbus:
-        slaveSelect = input("Which Arduino (1-3): ")
-        cmd = input("Enter command: ")
-        if slaveSelect == "1":
-            slaveAddress = I2C_SLAVE_ADDRESS
-        elif slaveSelect == "2":
-            slaveAddress = I2C_SLAVE2_ADDRESS
-        elif slaveSelect == "3":
-            slaveAddress = I2C_SLAVE3_ADDRESS
-        else:
-            # quit if you messed up
-            print(slaveSelect== "1")
-            print(type(slaveSelect))
-            print("no slave selected")
-            quit()
-        BytesToSend = ConvertStringsToBytes(cmd)
-        print("Sent " + str(slaveAddress) + " the " + str(cmd) + " command.")
-        print(BytesToSend )
-        I2Cbus.write_i2c_block_data(slaveAddress, 0x00, BytesToSend)
-        time.sleep(0.5)
-        while True:
-            try:
-                data=I2Cbus.read_i2c_block_data(slaveAddress,0x00,16)
-                print("recieve from slave:")
-                print(data)
-            except:
-                print("remote i/o error")
-                time.sleep(0.5)
-    return 0
+import sys
+
+bus = smbus.SMBus(1)
+address = 0x04
+
+def main():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    status = False
+    while 1:
+        gpio.output(17, status)
+        status = not status
+        bus.write_byte(address, 1 if status else 0)
+        print "Arduino answer to RPI:", bus.read_byte(address)
+        time.sleep(1)
+
 if __name__ == '__main__':
-     try:
-        main(sys.argv)
-     except KeyboardInterrupt:
-        print("program was stopped manually")
-     input()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print 'Interrupted'
+        gpio.cleanup()
+        sys.exit(0)
