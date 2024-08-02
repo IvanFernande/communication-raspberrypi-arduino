82a9d3f448b116142dba245254476e616367c03d
Iván Fernández Gómez <149154386+IvanFernande@users.noreply.github.com>
2024-08-02 23:32:43 +0200

    hc-05.py

--- BT/hc-05.py
+++ BT/hc-05.py
@@ -1,36 +1,36 @@
 import serial
 import time
 
-# Configuración inicial del puerto serial
+# Initial configuration of the serial port
 ser = serial.Serial(
-    port='/dev/rfcomm0',  # Reemplaza con el puerto correcto en tu Raspberry Pi
+    port='/dev/rfcomm0',  # Replace with the correct port on your Raspberry Pi
     baudrate=9600,
     timeout=1
 )
 
-# Función para enviar datos al HC-05
+# Function to send data to the HC-05
 def enviar_datos(data):
-    ser.write(data.encode())
+    ser.write(data.encode())  # Encode and write the data to the serial port
 
-# Función para recibir datos del HC-05
+# Function to receive data from the HC-05
 def recibir_datos():
     while True:
-        if ser.inWaiting() > 0:
-            incoming_data = ser.readline().decode().strip()
-            print("Datos recibidos:", incoming_data)
-            break
+        if ser.inWaiting() > 0:  # Check if there is data waiting to be read
+            incoming_data = ser.readline().decode().strip()  # Read, decode, and strip the incoming data
+            print("Data received:", incoming_data)  # Print the received data
+            break  # Exit the loop after receiving data
 
-# Ejemplo de uso
+# Example usage
 if __name__ == '__main__':
     try:
         while True:
-            # Enviar un mensaje
-            enviar_datos('Hola desde Raspberry Pi')
-            time.sleep(1)
+            # Send a message
+            enviar_datos('Hello from Raspberry Pi')
+            time.sleep(1)  # Wait for 1 second
 
-            # Recibir mensaje
-                      recibir_datos()
+            # Receive message
+            recibir_datos()
 
     except KeyboardInterrupt:
-        print("Programa terminado por el usuario")
-        ser.close()
+        print("User terminated programme")  # Print a message when the program is terminated by the user
+        ser.close()  # Close the serial port
